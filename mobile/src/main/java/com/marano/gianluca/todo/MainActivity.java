4d48c4112257a1184f70b96a3cd242bf56ca5b83
Gianluca Marano <gianluca.marano1@gmail.com>
2017-02-20 10:32:20 +0100

    primo commit

---
+++ mobile/src/main/java/com/marano/gianluca/todo/MainActivity.java
@@ -0,0 +1,10 @@
+package com.marano.gianluca.todo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gianluca Marano on 20/02/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
